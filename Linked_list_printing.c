244653396ac8e20e2c0e6bfd09c08cc8d404f06b
Shakira Islam <140553148+ShakirArt@users.noreply.github.com>
2024-05-22 19:12:55 +0600

    Add files via upload

---
+++ Linked_list_printing.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+int main(){
+    struct node *head, *newnode;
+    head = malloc(sizeof(struct node));
+    head -> data = 45;
+    head -> next = 0;
+    printf("1st - %d\n",head -> data);
+    newnode = malloc(sizeof(struct node));
+    newnode -> data = 80;
+    newnode -> next = 0;
+    head -> next = newnode;
+    printf("2nd - %d\n",newnode -> data);
+    newnode = malloc(sizeof(struct node));
+    newnode -> data = 78;
+    newnode -> next = 0;
+    head -> next -> next = newnode;
+    printf("3rd - %d\n",newnode -> data);
+    newnode = malloc(sizeof(struct node));
+    newnode -> data = 7;
+    newnode -> next = 0;
+    head -> next -> next -> next = newnode;
+    printf("4th - %d\n",newnode -> data);
+    print_data(head);
+    return 0;
+}
+
+void print_data(struct node *head) {
+    struct node *ptr = NULL;
+    ptr = head;
+    if(head == NULL){
+        printf("Linked List is empty");
+    }
+    while(ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+
+
+
+
