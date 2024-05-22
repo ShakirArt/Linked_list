14f73b6aec3341cc2aa9739a9651f27a99193443
Shakira Islam <140553148+ShakirArt@users.noreply.github.com>
2024-05-22 18:28:26 +0600

    Add files via upload

---
+++ Linked_list_Implimentation_single.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+int main(){
+    struct node *head, *newnode;
+    head = malloc(sizeof(struct node));
+    head -> data = 45;
+    head -> next = 0;
+    printf("1st - %d\n",head -> data);
+    newnode = malloc(sizeof(struct node));
+    newnode -> data = 80;
+    newnode -> next = 0;
+    head -> next = newnode;
+    printf("2nd - %d\n",newnode -> data);
+    newnode = malloc(sizeof(struct node));
+    newnode -> data = 78;
+    newnode -> next = 0;
+    head -> next -> next = newnode;
+    printf("3rd - %d\n",newnode -> data);
+    newnode = malloc(sizeof(struct node));
+    newnode -> data = 7;
+    newnode -> next = 0;
+    head -> next -> next -> next = newnode;
+    printf("4th - %d\n",newnode -> data);
+    return 0;
+}
+
+
+
+
+
+
